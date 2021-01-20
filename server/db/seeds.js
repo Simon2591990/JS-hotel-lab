371f4ba4248f3e8fc78274095be92f4d2a8d4782
Simon Charters <simon259@hotmail.co.uk>
2021-01-20 10:19:41 +0000

    added seeds

---
+++ server/db/seeds.js
@@ -0,0 +1,27 @@
+use hotel_database;
+
+db.dropDatabase();
+
+db.guests.insertMany([
+    {
+        name: "Sandra McSandraface",
+        emailAddress: "sandrascoolemail@hotmail.com",
+        checkedIn: false
+    },
+    {
+        name: "Lady Gaga",
+        emailAddress: "lady_gaga@yahoo.co.uk",
+        checkedIn: true
+    },
+    {
+        name: "Guesty McGuestface",
+        emailAddress: "mcguestface@hotmail.co.uk",
+        checkedIn: false
+    },
+    {
+        name: "Visity McVisitface",
+        emailAddress: "mcvistface@hotmail.com",
+        checkedIn: false
+    }
+])
+
